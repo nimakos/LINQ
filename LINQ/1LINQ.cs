6fdca88ce1208c2545b2c7515d2a7fab82f595c2
Vangelis Nikolis <nimakos@hotmail.com>
2017-10-28 16:22:12 +0300

    Add project files.

---
+++ LINQ/1LINQ.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class _1LINQ
+    {
+        long memoryBefore = GC.GetTotalMemory(true);
+        // The Three Parts of a LINQ Query:
+        //  1. Data source.
+        int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+
+        public void create_the_query()
+        {
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            // 3. Query execution.
+            foreach (int i in numQuery)
+            {
+                Console.Write("{0,2} ", i);
+            }
+            long memoryAfter = GC.GetTotalMemory(false);
+            Console.ReadLine();
+            Console.WriteLine("Memory Used  = \t" +
+                string.Format(((memoryAfter - memoryBefore) / 1000).ToString(), "n") + "kb");
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            _1LINQ query = new _1LINQ();
+            query.create_the_query();
+
+
+            _2LINQ query2 = new _2LINQ();
+            query2.Create_the_query();
+
+        }
+    }
+}
